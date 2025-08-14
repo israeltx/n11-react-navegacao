289888c348fe483a4e0e14f1d37e8044104cfe87
Israel Teixeira <israeltx@gmail.com>
2025-08-14 13:49:27 -0300

    Set parameters that are shared among routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,14 +4,18 @@ import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
 import { NotFound } from "../pages/NotFound";
 import { Details } from "../pages/Details";
+import { Layout } from "../components/Layout";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" index element={<Home />}/>
-      <Route path="/products"  element={<Products />}/>
-      <Route path="*"  element={<NotFound />}/>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<Home />}/>
+        <Route path="/products"  element={<Products />}/>
+      </Route>
+
       <Route path="/details/:id"  element={<Details />}/>
+      <Route path="*"  element={<NotFound />}/>
     </Routes>
   )
 }
