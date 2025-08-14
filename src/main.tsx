092c6c8fadb55a881623ca73cd691a40831bf296
Israel Teixeira <israeltx@gmail.com>
2025-08-14 11:45:57 -0300

    Create the 1st route

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { AppRoutes } from './routes/appRoutes'
 
 import './main.css'
 
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
 )
