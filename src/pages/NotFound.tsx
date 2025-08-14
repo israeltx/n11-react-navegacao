5f1c6e00daa982b224bf9d79488d8377b2c8d816
Israel Teixeira <israeltx@gmail.com>
2025-08-14 12:05:10 -0300

    Create a 404 page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>Página não existe</h1>
+      <a href="/">Voltar</a>
+    </div>
+  )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 
 import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
+import { NotFound } from "../pages/NotFound";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />}/>
       <Route path="/products"  element={<Products />}/>
+      <Route path="*"  element={<NotFound />}/>
     </Routes>
   )
 }
